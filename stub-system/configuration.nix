384a381115dd0ee6794576f7c9d392469d2f6b15
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-26 23:11:55 +0800

    Problem: Stubbing a whole system is overkill
    NixOS has a mode for generating a container.
    
    Solution: Use container mode for the stubbed system.

--- stub-system/configuration.nix
+++ stub-system/configuration.nix
@@ -5,10 +5,5 @@
   imports = [
   ];
 
-  # Provide minimal information so that NixOS believes it has a system to build
-  boot.loader.grub.devices = [ "/dev/sda" ];
-  fileSystems."/" = {
-    device = "/dev/mapper/vg0/lv0";
-    fsType = "ext4";
-  };
+  boot.isContainer = true;
 }
