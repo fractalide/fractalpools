63c6d416f808d17f38c3dc4587f66c53efd0b95f
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-28 20:11:31 +0800

    Problem: No pinned nixpkgs
    Solution: Pin nixpkgs, use in release.nix.
    
    Issues: Not using pinned nixpkgs in module.

--- release.nix
+++ release.nix
@@ -6,7 +6,8 @@ let
     inherit (import path { configuration = ./stub-system/configuration.nix; }) system;
   };
 in
-genJobs <nixpkgs/nixos> // {
+genJobs "${(import ./pins/nixpkgs)}/nixos" // {
+  unstable = genJobs <nixpkgs/nixos>;
   oldstable = genJobs <nixos-oldstable/nixos>;
   stable = genJobs <nixos-stable/nixos>;
 } // (import <nixpkgs> {}).lib.optionalAttrs isTravis {
