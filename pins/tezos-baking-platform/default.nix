114d8a0203bc26ca0e7628eb27c422394b59086c
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-10-16 18:33:31 +0800

    Problem: empty: Using impure <nixpkgs> reference
    Solution: Bump tezos-baking-platform.
    
    Closes #30

--- pins/tezos-baking-platform/default.nix
+++ pins/tezos-baking-platform/default.nix
@@ -1,5 +1,2 @@
-(import <nixpkgs> {}).fetchgit {
-  url = "https://gitlab.com/clacke/tezos-baking-platform";
-  rev = "bd00a77315fca9ef3a52012b74c34592a87cb1ae";
-  sha256 = "02zhjx7x16f3qiln22d4dkjwwdwyn1yvjn24608l4ahhi3dnsjrs";
-}
+(import <nixpkgs> {}).fetchgit
+  (builtins.removeAttrs (builtins.fromJSON (builtins.readFile ./default.json)) [ "date" ])
