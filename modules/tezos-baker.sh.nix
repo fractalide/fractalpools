db9ae68de2de97965409638505efc291eb5d0705
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-08 00:02:34 +0800

    Problem: tezos-init needs node running
    Generating the keys for the baker is done with an RPC call.
    
    Solution: Move baker key gen to the baker service.
    
    As an added benefit we don't need to switch users, because the service
    is already running as the non-root user.

--- modules/tezos-baker.sh.nix
+++ modules/tezos-baker.sh.nix
@@ -10,6 +10,11 @@ set -e
 set -u
 set -o pipefail
 
+if ! ${kit}/bin/tezos-client --base-dir "${bakerDir}" list known addresses | grep -E '^${bakerAddressAlias}: '; then
+  ${kit}/bin/tezos-client --base-dir "${bakerDir}" gen keys "${bakerAddressAlias}"
+  ${kit}/bin/tezos-client --base-dir "${bakerDir}" list known addresses | grep -E '^${bakerAddressAlias}: '
+fi
+
 exec ${kit}/bin/tezos-baker-002-PsYLVpVv --base-dir "${bakerDir}" --addr localhost --port ${toString (8732 + index)} \
   run with local node "${configDir}" "${bakerAddressAlias}"
 ''
