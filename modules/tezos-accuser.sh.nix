a7a4281011e65dfe9962a8e17580401bbe4794d3
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-11-26 17:42:11 +0800

    Problem: accuser baker endorser: old protocol
    Solution: Run the binaries for the new protocol.
    
    The old protocol is safe to run in parallel, the services will become
    inactive at fork time. We'll remove them again when this has happened.
    
    I'm adding some copy-paste for this emergency fix. Next time this
    happens, we'll have something neater in store.

--- modules/tezos-accuser.sh.nix
+++ modules/tezos-accuser.sh.nix
@@ -8,6 +8,6 @@ set -e
 set -u
 set -o pipefail
 
-exec ${kit}/bin/tezos-accuser-002-PsYLVpVv --base-dir "${bakerDir}" --addr localhost --port ${toString (8732 + index)} \
+exec ${kit}/bin/tezos-accuser-003-PsddFKi3 --base-dir "${bakerDir}" --addr localhost --port ${toString (8732 + index)} \
   run
 ''
